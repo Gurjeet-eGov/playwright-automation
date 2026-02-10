1ef574c0b138af83a1847085558c78e1a7f45cec
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2026-02-10 16:56:58 +0530

    TL dashboard ui

--- tests/ui/test_demo.py
+++ tests/ui/test_demo.py
@@ -1,30 +1,35 @@
-import pytest, time
+import pytest, time, re
 
 from playwright.sync_api import Page
+from utils import helpers
 
-def test_demo(context):
-    page = context.new_page()
+def test_demo(page: Page):
+
+    # Language selection
+    # page = context.new_page()
     page.goto("https://unified-demo.digit.org/digit-ui/employee/user/language-selection")
 
-    language_buttons = page.locator(".language-button-container button")
-
-    language_buttons.first.wait_for()
+    # Get all available languages
+    language_dd = page.locator(".language-button-container button")
+    language_dd.first.wait_for()
     languages = []
-    for i in range(language_buttons.count()):
-        button_text = language_buttons.nth(i).inner_text()
+    for i in range(language_dd.count()):
+        button_text = language_dd.nth(i).inner_text()
         languages.append(button_text)
-
     print("\nAvailable languages:", languages)
+    
+    # Language selection
     locale = languages[0]
     print("\nSelected languages:", locale)
-    language_buttons.get_by_text(locale).click()
-
+    language_dd.get_by_text(locale).click()
     page.get_by_role("button").get_by_text("Continue").click()
 
+
+    # Employee Login
     page.locator("input[name='username']").fill("TL_SU")
     page.locator("input[name='password']").fill("eGov@1234")
 
-
+    # City selection
     dropdown_wrapper = page.locator(".employee-select-wrap.login-city-dd")
     dropdown_wrapper.click()
     options_box = page.locator("#jk-dropdown-unique")
@@ -33,64 +38,43 @@ def test_demo(context):
 
     page.locator("button[type='submit']").click()
 
+    # Wait for navigation to employee landing page
     page.wait_for_url("**/digit-ui/employee")
 
+    # Extract page text
     body = page.locator("[id='root']")
     text = body.inner_text().split('\n')
-
+    # Sidebar interaction
     sidebar = page.locator(".sidebar")
     sidebar.hover()
-    
+    # Sidebar click to expand (TL module)
     sidebar.locator("[data-for='jk-side-TRADE_LICENSE']").click()
     sidebar_text = sidebar.inner_text().split('\n')
 
     print("\nLogged in page text:", text, type(text))
     print("\nSidebar text:", sidebar_text, type(sidebar_text))
 
-    text.extend(sidebar_text)
-    print("\nFinal:", text)
+    body.locator(".employee-app-container").hover()
+    page.get_by_role("link", name="Dashboard").click()
+    dss_body = page.locator("#divToPrint")
+    dss_body.wait_for(state="visible", timeout=15000)
+    dss_text = dss_body.inner_text()
+    dss_text = re.split(r'[\n\t]+', dss_text)
+    dss_text = [item.strip() for item in dss_text if item.strip()]
+    print("\nDSS text:", dss_text, type(dss_text))
+
+
+    loc_list = helpers.list_cleanup(text + sidebar_text + dss_text)
+    print("\nFinal:", loc_list)
+
+    # Find localization leaks
+    leaks = helpers.find_loc_codes(loc_list, 'resources/source.json')
+    print("\nLocalization leaks:", leaks)
+    helpers.write_csv(leaks, 'tl_locales.csv')
+    helpers.write_json(leaks, 'tl_locales.json')
 
     time.sleep(3)
     assert True
 
-
-
-
-@pytest.mark.localization
-def test_emp_login(page: Page):
-    # page = context.new_page()
-    page.goto("https://unified-demo.digit.org/digit-ui/employee/user/language-selection")
-
-    language_buttons = page.locator(".language-button-container button")
-
-    language_buttons.first.wait_for()
-    languages = []
-    for i in range(language_buttons.count()):
-        button_text = language_buttons.nth(i).inner_text()
-        languages.append(button_text)
-
-    print("\nAvailable languages:", languages)
-    locale = languages[1]
-    print("\nSelected languages:", locale)
-    language_buttons.get_by_text(locale).click()
-
-    page.get_by_role("button").get_by_text("Continue").click()
-
-    page.locator("input[name='username']").fill("TL_SU")
-    page.locator("input[name='password']").fill("eGov@1234")
-
-
-    dropdown_wrapper = page.locator(".employee-select-wrap.login-city-dd")
-    dropdown_wrapper.click()
-    options_box = page.locator("#jk-dropdown-unique")
-    options_box.wait_for(state="visible", timeout=5000)
-    options_box.locator(".profile-dropdown--item").nth(1).click()
-
-    page.locator("button[type='submit']").click()
-
-
-
-    time.sleep(5)
-    assert True
 
 
---
+++ utils/helpers.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import csv, json
+
+def list_cleanup(data_list):
+    # This keeps the item only if it is NOT an empty string or a dash
+    sp_chars = ['', '-', ' ']
+    return [item for item in data_list if item not in sp_chars]
+
+def find_loc_codes(ui_strings, source_json_path):
+    # 1. Load the Source JSON
+    source_json_path = 'output/resources/source.json'
+    with open(source_json_path, 'r', encoding='utf-8') as f:
+        source_data = json.load(f)
+
+    # 2. Create a set of all valid localized "messages"
+    # We use a set for lightning-fast lookups
+    valid_messages = {item['message'] for item in source_data}
+
+    leaks = []
+
+    # 3. Compare UI strings against the valid messages
+    for string in ui_strings:
+        # Check if the UI string is missing from the message whitelist
+        if string not in valid_messages:
+            leaks.append(string)
+        # Else: it exists in 'message', so we skip it (localized)
+
+    return leaks
+
+def write_csv(data, filename):
+    # Convert the list to a DataFrame
+    df = pd.DataFrame(data, columns=["Locales"])
+    
+    # Save to CSV
+    df.to_csv('output/resources/'+filename, index=False, quoting=csv.QUOTE_ALL , encoding='utf-8')
+
+def write_json(data, filename):
+    with open('output/resources/'+filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
